ade85755ffb846454b431f0373c84d4384a2c743
Aare Undo <aare@carto.com>
2017-04-12 14:36:47 +0300

    iOS compilation fixes

--- mobile_style_editor/Google/GoogleExtensions.cs
+++ mobile_style_editor/Google/GoogleExtensions.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Threading.Tasks;
+#if __ANDROID__
 using Android.Gms.Common.Apis;
 using Android.Gms.Drive;
 using Android.Gms.Drive.Query;
 using Android.Runtime;
+#elif __IOS__
+#endif
 
 namespace mobile_style_editor
 {
@@ -12,9 +15,13 @@ namespace mobile_style_editor
 		/* From:
 		 * https://github.com/xamarin/GooglePlayServicesComponents/blob/master/drive/source/Additions/IPendingResultExtensions.cs
 		 */
+#if __ANDROID__
 		public static async Task<IDriveApiMetadataBufferResult> QueryAsync(this IDriveApi api, GoogleApiClient apiClient, QueryClass query)
 		{
 			return (await api.Query(apiClient, query)).JavaCast<IDriveApiMetadataBufferResult>();
 		}
+#elif __IOS__
+#endif
+
 	}
 }
