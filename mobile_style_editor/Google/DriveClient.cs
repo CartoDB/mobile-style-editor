794bef2c153940c86876b0ed2c50cf6a91f6a310
Aare Undo <aare@carto.com>
2017-04-13 12:13:15 +0300

    Driveclient cleanup

--- mobile_style_editor/Google/DriveClient.cs
+++ mobile_style_editor/Google/DriveClient.cs
@@ -20,6 +20,8 @@ namespace mobile_style_editor
 #if __ANDROID__
 	public class DriveClient : Java.Lang.Object, GoogleApiClient.IConnectionCallbacks, GoogleApiClient.IOnConnectionFailedListener
 	{
+		public static DriveClient Instance = new DriveClient();
+
 		/*
 		 * Requires activity reference. Thankfully, Xamarin.Forms.Forms.Context is the default MainActivity,
 		 * when allowed, the activity's OnActivityResult will be called
@@ -30,7 +32,10 @@ namespace mobile_style_editor
 		 * 
 		 */
 
-		public static DriveClient Instance = new DriveClient();
+		public const int RequestCode_RESOLUTION = 1;
+		public const int RequestCode_OPENER = 2;
+
+		public const string Response_DRIVEID = "response_drive_id";
 
 		public EventHandler<DownloadEventArgs> DownloadComplete;
 
@@ -47,7 +52,6 @@ namespace mobile_style_editor
 			GoogleApiClient.Builder builder = new GoogleApiClient.Builder(context, this, this);
 			builder.AddApi(DriveClass.API);
 			builder.AddScope(DriveClass.ScopeFile);
-			//builder.AddScope(new Scope("https://www.googleapis.com/auth/drive"));
 
 			client = builder.Build();
 		}
@@ -59,59 +63,17 @@ namespace mobile_style_editor
 
 		public void OnConnected(Bundle connectionHint)
 		{
-			Test();
+			OpenPicker();
 		}
 
-		public async void Test()
+		static readonly string[] MimeType = { "application/zip" };
+
+		public void OpenPicker()
 		{
-			IntentSender intentSender = DriveClass.DriveApi
-				.NewOpenFileActivityBuilder()
-				.SetMimeType(new string[] { "application/zip" })
-				.Build(client);
-			((Activity)context).StartIntentSenderForResult(
-					intentSender, RequestCode_OPENER, null, 0, 0, 0);
-			return;
+			OpenFileActivityBuilder builder = DriveClass.DriveApi.NewOpenFileActivityBuilder();
+			builder.SetMimeType(MimeType);
 
-			// Method for creating a folder. Non-functional
-			MetadataChangeSet.Builder changeset = new MetadataChangeSet.Builder();
-			changeset.SetTitle("testTitle");
-			changeset.SetDescription("testDescription");
-
-			IDriveFolder appFolder = DriveClass.DriveApi.GetAppFolder(client);
-			if (appFolder != null)
-			{
-				await appFolder.CreateFolder(client, changeset.Build());
-			}
-			else
-			{
-				Console.WriteLine(":( appFolder is still null");
-			}
-
-			QueryClass query = new QueryClass.Builder().Build();
-
-			// Method 1 for getting Drive files/folders
-			DriveClass.DriveApi.Query(client, query).SetResultCallback(new QueryCallback());
-
-			// Method 2 for getting Drive files/folders
-			IDriveFolder folder = DriveClass.DriveApi.GetRootFolder(client);
-			IDriveApiMetadataBufferResult result1 = (await folder.ListChildren(client)).JavaCast<IDriveApiMetadataBufferResult>();
-			IEnumerator<Metadata> list1 = result1.MetadataBuffer.GetEnumerator();
-
-			while (list1.MoveNext())
-			{
-				Metadata current = list1.Current;
-				Console.WriteLine(current);
-			}
-
-			// Method 3 for getting Drive files/folders
-			IDriveApiMetadataBufferResult result2 = await DriveClass.DriveApi.QueryAsync(client, query);
-			IEnumerator<Metadata> list2 = result2.MetadataBuffer.GetEnumerator();
-
-			while (list2.MoveNext())
-			{
-				Metadata current = list2.Current;
-				Console.WriteLine(current);
-			}
+			((Activity)context).StartIntentSenderForResult(builder.Build(client), RequestCode_OPENER, null, 0, 0, 0);
 		}
 
 		public void Download(DriveId driveId)
@@ -135,11 +97,6 @@ namespace mobile_style_editor
 			});
 		}
 
-		public const int RequestCode_RESOLUTION = 1;
-		public const int RequestCode_OPENER = 2;
-
-		public const string Response_DRIVEID = "response_drive_id";
-
 		public void OnConnectionFailed(ConnectionResult result)
 		{
 			if (result.HasResolution)
@@ -161,25 +118,8 @@ namespace mobile_style_editor
 
 		public void OnConnectionSuspended(int cause)
 		{
+			// TODO When is this called?
 			throw new NotImplementedException();
-		}
-	}
-
-	public class QueryCallback : Java.Lang.Object, IResultCallback
-	{
-		public void OnResult(Java.Lang.Object result)
-		{
-			IDriveApiMetadataBufferResult parsed = result.JavaCast<IDriveApiMetadataBufferResult>();
-			Console.WriteLine(parsed);
-		}
-	}
-
-	public class FetchIdCallback : Java.Lang.Object, IResultCallback
-	{
-		public void OnResult(Java.Lang.Object result)
-		{
-			IDriveApiDriveIdResult parsed = result.JavaCast<IDriveApiDriveIdResult>();
-			Console.WriteLine(parsed);
 		}
 	}
 
@@ -187,3 +127,63 @@ namespace mobile_style_editor
 
 #endif
 }
+
+// TODO REMOVE old, unused logic
+//	public class QueryCallback : Java.Lang.Object, IResultCallback
+//	{
+//		public void OnResult(Java.Lang.Object result)
+//		{
+//			IDriveApiMetadataBufferResult parsed = result.JavaCast<IDriveApiMetadataBufferResult>();
+//			Console.WriteLine(parsed);
+//		}
+//	}
+
+//	public class FetchIdCallback : Java.Lang.Object, IResultCallback
+//	{
+//		public void OnResult(Java.Lang.Object result)
+//		{
+//			IDriveApiDriveIdResult parsed = result.JavaCast<IDriveApiDriveIdResult>();
+//			Console.WriteLine(parsed);
+//		}
+//	}
+
+//			// Method for creating a folder. Non-functional
+//			MetadataChangeSet.Builder changeset = new MetadataChangeSet.Builder();
+//	changeset.SetTitle("testTitle");
+//			changeset.SetDescription("testDescription");
+
+//			IDriveFolder appFolder = DriveClass.DriveApi.GetAppFolder(client);
+//			if (appFolder != null)
+//			{
+//				await appFolder.CreateFolder(client, changeset.Build());
+//			}
+//			else
+//			{
+//				Console.WriteLine(":( appFolder is still null");
+//			}
+
+//			QueryClass query = new QueryClass.Builder().Build();
+
+//// Method 1 for getting Drive files/folders
+//DriveClass.DriveApi.Query(client, query).SetResultCallback(new QueryCallback());
+
+//			// Method 2 for getting Drive files/folders
+//			IDriveFolder folder = DriveClass.DriveApi.GetRootFolder(client);
+//IDriveApiMetadataBufferResult result1 = (await folder.ListChildren(client)).JavaCast<IDriveApiMetadataBufferResult>();
+//IEnumerator<Metadata> list1 = result1.MetadataBuffer.GetEnumerator();
+
+//			while (list1.MoveNext())
+//			{
+//				Metadata current = list1.Current;
+//Console.WriteLine(current);
+//			}
+
+//			// Method 3 for getting Drive files/folders
+//			IDriveApiMetadataBufferResult result2 = await DriveClass.DriveApi.QueryAsync(client, query);
+//IEnumerator<Metadata> list2 = result2.MetadataBuffer.GetEnumerator();
+
+//			while (list2.MoveNext())
+//			{
+//				Metadata current = list2.Current;
+//Console.WriteLine(current);
+//			}
