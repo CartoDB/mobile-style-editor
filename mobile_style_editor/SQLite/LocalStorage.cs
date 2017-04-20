6cb36b780a3ac4f9209c11e732da57e3c2b71a47
Aare Undo <aare@carto.com>
2017-04-20 11:27:26 +0300

    SQLite

---
+++ mobile_style_editor/SQLite/LocalStorage.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Collections.Generic;
+using SQLite;
+
+namespace mobile_style_editor
+{
+	public class LocalStorage
+	{
+		public static LocalStorage Instance = new LocalStorage();
+
+		SQLiteConnection db;
+
+		public List<StoredStyle> Styles { get { return db.Query<StoredStyle>("select * from StoredStyle"); } }
+
+		LocalStorage()
+		{
+			string folder = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+			const string file = "mobile_styles.db";
+
+			db = new SQLiteConnection(System.IO.Path.Combine(folder, file));
+
+			db.CreateTable<StoredStyle>();
+		}
+
+		public void AddStyle(string name, string path)
+		{
+			db.Insert(new StoredStyle { Name = name, Path = path });	
+		}
+	}
+}
---
+++ mobile_style_editor/SQLite/StoredStyle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace mobile_style_editor
+{
+	public class StoredStyle
+	{
+		[SQLite.PrimaryKey, SQLite.AutoIncrement]
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+
+		public string Path { get; set; }
+	}
+}
