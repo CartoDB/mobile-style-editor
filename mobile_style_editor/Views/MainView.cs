46ee650e1e40d502c71069031811442fa71bc691
Aare Undo <aare.undo@gmail.com>
2017-04-25 18:14:12 +0300

    cleanup

--- mobile_style_editor/Views/MainView.cs
+++ mobile_style_editor/Views/MainView.cs
@@ -109,7 +109,6 @@ namespace mobile_style_editor
 
 			System.Threading.Tasks.Task.Run(delegate
 			{
-				Console.WriteLine("Update map: starting");
 				BinaryData styleAsset = new BinaryData(data);
 
 				var package = new ZippedAssetPackage(styleAsset);
@@ -117,7 +116,7 @@ namespace mobile_style_editor
 
 				var source = new CartoOnlineTileDataSource(OSM);
 				var decoder = new MBVectorTileDecoder(styleSet);
-
+                
 				var layer = new VectorTileLayer(source, decoder);
 				Device.BeginInvokeOnMainThread(delegate
 				{
