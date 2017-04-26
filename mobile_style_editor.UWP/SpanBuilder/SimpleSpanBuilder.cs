27e03682f95be55cab339f7835ef7052042103fe
Aare Undo <aare.undo@gmail.com>
2017-04-26 17:08:41 +0300

    uwp code block formatting

--- mobile_style_editor.UWP/SpanBuilder/SimpleSpanBuilder.cs
+++ mobile_style_editor.UWP/SpanBuilder/SimpleSpanBuilder.cs
@@ -4,16 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.UI;
+using Windows.UI.Text;
+using Windows.UI.Xaml.Controls;
 
 namespace mobile_style_editor.UWP
 {
     public class SimpleSpanBuilder
     {
+        RichEditBox box;
+
+        public SimpleSpanBuilder(RichEditBox field)
+        {
+            box = field;
+        }
+
         string text = "";
 
-        public void Append(string text, Color color, float size)
+        public void Append(string addition, Color color, float size)
         {
-            this.text += text;
+            int start = text.Length;
+            int end = start + addition.Length;
+            text += addition;
+
+            ITextRange range = null;
+            Xamarin.Forms.Device.BeginInvokeOnMainThread(delegate
+            {
+                range = box.Document.GetRange(start, end);
+                range.CharacterFormat.ForegroundColor = color;
+            });
         }
 
         public string Build()
