36e13a8582aae8c5c7ad2ae15346607511138723
Aare Undo <aare@carto.com>
2017-05-05 16:20:59 +0300

    droid: kill previous toast if it isn't finished yet

--- mobile_style_editor/Utils/Toast.cs
+++ mobile_style_editor/Utils/Toast.cs
@@ -3,10 +3,20 @@ namespace mobile_style_editor
 {
 	public class Toast
 	{
+#if __ANDROID__
+		static Android.Widget.Toast toast;
+#endif
 		public static void Show(string text)
 		{
 #if __ANDROID__
-			Android.Widget.Toast.MakeText(Xamarin.Forms.Forms.Context, text, Android.Widget.ToastLength.Short).Show();
+
+			if (toast != null)
+			{
+				toast.Cancel();
+			}
+
+			toast = Android.Widget.Toast.MakeText(Xamarin.Forms.Forms.Context, text, Android.Widget.ToastLength.Short);
+			toast.Show();
 #elif __IOS__
 			ToastIOS.Toast.MakeText(text).SetFontSize(12f).Show();
 #elif __UWP__
