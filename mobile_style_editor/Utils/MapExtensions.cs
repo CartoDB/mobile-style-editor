e274c53edd5b0b92119b1f779a4a7c8804e3f894
Aare Undo <aare@carto.com>
2017-05-19 13:38:47 +0300

    basic validation logic

--- mobile_style_editor/Utils/MapExtensions.cs
+++ mobile_style_editor/Utils/MapExtensions.cs
@@ -14,17 +14,17 @@ namespace mobile_style_editor
 	{
 		const string OSM = "nutiteq.osm";
 
-		public static void Update(this MapView MapView, byte[] data, Action completed)
-		{
+        public static void Update(this MapView MapView, byte[] data, Action completed, Action<string> failed = null)
+        {
             System.Threading.Tasks.Task.Run(delegate
             {
                 BinaryData styleAsset = new BinaryData(data);
 
-				var package = new ZippedAssetPackage(styleAsset);
-				var styleSet = new CompiledStyleSet(package);
+                var package = new ZippedAssetPackage(styleAsset);
+                var styleSet = new CompiledStyleSet(package);
 
-				// UWP doesn't have a version released where simply changing the style set is supported,
-				// need to clear layers and recreate the entire thing
+                // UWP doesn't have a version released where simply changing the style set is supported,
+                // need to clear layers and recreate the entire thing
 #if __UWP__
 				MapView.Layers.Clear();
 		
@@ -41,34 +41,35 @@ namespace mobile_style_editor
                     }
 				});
 #else
-				if (MapView.Layers.Count == 0)
-				{
-					var source = new CartoOnlineTileDataSource(OSM);
-					var decoder = new MBVectorTileDecoder(styleSet);
+                if (MapView.Layers.Count == 0)
+                {
+                    var source = new CartoOnlineTileDataSource(OSM);
+                    var decoder = new MBVectorTileDecoder(styleSet);
 
-					var layer = new VectorTileLayer(source, decoder);
-					Device.BeginInvokeOnMainThread(delegate
-					{
-						MapView.Layers.Add(layer);
-						if (completed != null)
-						{
-							completed();
-						}
-					});
-				}
-				else
-				{
-					var decoder = (MBVectorTileDecoder)(MapView.Layers[0] as VectorTileLayer).TileDecoder;
-
-					Device.BeginInvokeOnMainThread(delegate
-					{
-						decoder.CompiledStyle = styleSet;
-						if (completed != null)
-						{
+                    var layer = new VectorTileLayer(source, decoder);
+                    Device.BeginInvokeOnMainThread(delegate
+                    {
+                        MapView.Layers.Add(layer);
+                        if (completed != null)
+                        {
                             completed();
-						}
-					});
-				}
+                        }
+                    });
+                }
+                else
+                {
+                    var decoder = (MBVectorTileDecoder)(MapView.Layers[0] as VectorTileLayer).TileDecoder;
+
+                    Device.BeginInvokeOnMainThread(delegate
+                    {
+                        decoder.CompiledStyle = styleSet;
+
+                        if (completed != null)
+                        {
+                            completed();
+                        }
+                    });
+                }
 #endif
             });
         }
