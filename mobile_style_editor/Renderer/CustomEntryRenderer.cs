3a4e7095cb65d0aa8039aaa502c49bf3a391701d
Aare Undo <aare@carto.com>
2017-05-30 14:08:05 +0300

    custom android textview renderer

---
+++ mobile_style_editor/Renderer/CustomEntryRenderer.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using mobile_style_editor;
+using Xamarin.Forms;
+
+#if __IOS__
+using Xamarin.Forms.Platform.iOS;
+#elif __ANDROID__
+using Xamarin.Forms.Platform.Android;
+#elif __UWP__
+using Xamarin.Forms.Platform.UWP;
+#endif
+
+[assembly: ExportRenderer(typeof(BaseEntry), typeof(CustomEntryRenderer))]
+namespace mobile_style_editor
+{
+    public class CustomEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+#if __ANDROID__
+            if (Control != null)
+            {
+                Control.SetBackgroundColor(Android.Graphics.Color.Transparent);
+                Control.SetPadding(5, 0, 0, 0);
+                Control.Gravity = Android.Views.GravityFlags.CenterVertical;
+            }
+#endif
+        }
+
+    }
+}
