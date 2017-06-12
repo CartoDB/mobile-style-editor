14c9ba1fe348eb6c1e5cfd10e02b2ec0550a4cb4
Aare Undo <aare@carto.com>
2017-06-12 10:28:08 +0300

    cleanup; refactored CartoCSSParser into separate class

---
+++ mobile_style_editor/Views/Main/Editor/CartoCSSParser.cs
@@ -0,0 +1,109 @@
+﻿
+using System;
+using Xamarin.Forms;
+
+namespace mobile_style_editor
+{
+    public class CartoCSSParser
+    {
+        public static
+#if __ANDROID__
+        Android.Text.SpannableStringBuilder
+#elif __IOS__
+        Foundation.NSMutableAttributedString
+#elif __UWP__
+
+#endif
+        Parse(string text)
+        {
+			string[] lines = text.Split('\n');
+
+#if __ANDROID__
+			var builder = new Droid.SimpleSpanBuilder();
+#elif __IOS__
+
+            var builder = new iOS.AttributedTextBuilder();
+#elif __UWP__
+            var builder = new UWP.SimpleSpanBuilder(this);
+#endif
+			float size = 1f;
+
+			// White
+			Color generalColor = Color.White;
+			// Light gray
+			Color commentColor = Color.FromRgb(120, 120, 120);
+			// Carto green
+			Color blockHeaderColor = Color.FromRgb(145, 198, 112);
+			// Teal
+			Color constantColor = Color.FromRgb(0, 150, 150);
+
+			bool isInCommentBlock = false;
+
+			foreach (string line in lines)
+			{
+				string trimmed = line.Trim();
+				string withNewLine = line + "\n";
+
+				if (isInCommentBlock)
+				{
+					if (trimmed.Contains("*/"))
+					{
+						int nonCommentIndex = line.IndexOf("*/", StringComparison.Ordinal) + 2;
+						string comment = line.Substring(0, nonCommentIndex);
+						string nonComment = line.Substring(nonCommentIndex, line.Length - nonCommentIndex);
+
+						builder.Append(comment, commentColor.ToNativeColor(), size);
+						builder.Append(nonComment + "\n", generalColor.ToNativeColor(), size);
+						isInCommentBlock = false;
+					}
+					else
+					{
+						builder.Append(withNewLine, commentColor.ToNativeColor(), size);
+					}
+					continue;
+				}
+
+				if (trimmed.StartsWith("//", StringComparison.Ordinal))
+				{
+					builder.Append(withNewLine, commentColor.ToNativeColor(), size);
+				}
+				else if (trimmed.StartsWith("@", StringComparison.Ordinal))
+				{
+					builder.Append(withNewLine, constantColor.ToNativeColor(), size);
+				}
+				else if (trimmed.Contains("/*"))
+				{
+					int commentIndex = line.IndexOf("/*", StringComparison.Ordinal);
+					string nonComment = line.Substring(0, commentIndex);
+					string comment = line.Substring(commentIndex, line.Length - commentIndex);
+
+					builder.Append(nonComment, generalColor.ToNativeColor(), size);
+					builder.Append(comment + "\n", commentColor.ToNativeColor(), size);
+					isInCommentBlock = true;
+				}
+				else
+				{
+					if (trimmed.Contains("{"))
+					{
+						int bracketIndex = line.IndexOf("{", StringComparison.Ordinal);
+						string blockHeader = line.Substring(0, bracketIndex);
+						string remaining = line.Substring(bracketIndex, line.Length - bracketIndex);
+
+						builder.Append(blockHeader, blockHeaderColor.ToNativeColor(), size);
+						builder.Append(remaining + "\n", generalColor.ToNativeColor(), size);
+					}
+					else if (trimmed.Contains("#") || trimmed.Contains("["))
+					{
+						builder.Append(withNewLine, blockHeaderColor.ToNativeColor(), size);
+					}
+					else
+					{
+						builder.Append(withNewLine, generalColor.ToNativeColor(), size);
+					}
+				}
+			}
+
+            return builder.Build();
+        }
+    }
+}
--- mobile_style_editor/Views/Main/Editor/EditorField.cs
+++ mobile_style_editor/Views/Main/Editor/EditorField.cs
@@ -324,111 +324,25 @@ namespace mobile_style_editor
 
             System.Threading.Tasks.Task.Run(delegate
             {
-                string[] lines = text.Split('\n');
-
-#if __ANDROID__
-				var builder = new Droid.SimpleSpanBuilder();
-#elif __IOS__
-
-                var builder = new iOS.AttributedTextBuilder();
-#elif __UWP__
-            var builder = new UWP.SimpleSpanBuilder(this);
-#endif
-                float size = 1f;
-
-                // White
-                Color generalColor = Color.White;
-                // Light gray
-                Color commentColor = Color.FromRgb(120, 120, 120);
-                // Carto green
-                Color blockHeaderColor = Color.FromRgb(145, 198, 112);
-				// Teal
-                Color constantColor = Color.FromRgb(0, 150, 150);
-				
-                bool isInCommentBlock = false;
-
 #if __UWP__
-                Device.BeginInvokeOnMainThread(delegate
-                {
-                    Text = text;
-                });
+	            Device.BeginInvokeOnMainThread(delegate
+	            {
+	                Text = text;
+	            });
 #endif
-
-                foreach (string line in lines)
-                {
-                    string trimmed = line.Trim();
-                    string withNewLine = line + "\n";
-
-                    if (isInCommentBlock)
-                    {
-                        if (trimmed.Contains("*/"))
-                        {
-                            int nonCommentIndex = line.IndexOf("*/", StringComparison.Ordinal) + 2;
-                            string comment = line.Substring(0, nonCommentIndex);
-                            string nonComment = line.Substring(nonCommentIndex, line.Length - nonCommentIndex);
-
-                            builder.Append(comment, commentColor.ToNativeColor(), size);
-                            builder.Append(nonComment + "\n", generalColor.ToNativeColor(), size);
-                            isInCommentBlock = false;
-                        }
-                        else
-                        {
-                            builder.Append(withNewLine, commentColor.ToNativeColor(), size);
-                        }
-                        continue;
-                    }
-
-                    if (trimmed.StartsWith("//", StringComparison.Ordinal))
-                    {
-                        builder.Append(withNewLine, commentColor.ToNativeColor(), size);
-                    }
-                    else if (trimmed.StartsWith("@", StringComparison.Ordinal))
-                    {
-                        builder.Append(withNewLine, constantColor.ToNativeColor(), size);
-                    }
-                    else if (trimmed.Contains("/*"))
-                    {
-                        int commentIndex = line.IndexOf("/*", StringComparison.Ordinal);
-                        string nonComment = line.Substring(0, commentIndex);
-                        string comment = line.Substring(commentIndex, line.Length - commentIndex);
-
-                        builder.Append(nonComment, generalColor.ToNativeColor(), size);
-                        builder.Append(comment + "\n", commentColor.ToNativeColor(), size);
-                        isInCommentBlock = true;
-                    }
-                    else
-                    {
-                        if (trimmed.Contains("{"))
-                        {
-                            int bracketIndex = line.IndexOf("{", StringComparison.Ordinal);
-                            string blockHeader = line.Substring(0, bracketIndex);
-                            string remaining = line.Substring(bracketIndex, line.Length - bracketIndex);
-
-                            builder.Append(blockHeader, blockHeaderColor.ToNativeColor(), size);
-                            builder.Append(remaining + "\n", generalColor.ToNativeColor(), size);
-                        }
-                        else if (trimmed.Contains("#") || trimmed.Contains("["))
-                        {
-                            builder.Append(withNewLine, blockHeaderColor.ToNativeColor(), size);
-                        }
-                        else
-                        {
-                            builder.Append(withNewLine, generalColor.ToNativeColor(), size);
-                        }
-                    }
-                }
+				object result = CartoCSSParser.Parse(text);
 
                 Device.BeginInvokeOnMainThread(delegate
                 {
 #if __ANDROID__
-					TextFormatted = builder.Build();
+                    TextFormatted = (SpannableStringBuilder)result;
 
 					if (selection != -1)
 					{
 						SetSelection(selection);
 					}
 #elif __IOS__
-                    AttributedText = builder.Build();
+                    AttributedText = (Foundation.NSMutableAttributedString)result;
                     SetNeedsDisplay();
 
                     if (selection != -1)
@@ -439,10 +353,9 @@ namespace mobile_style_editor
 #elif __UWP__
                     Document.ApplyDisplayUpdates();
 #endif
+					System.Diagnostics.Debug.WriteLine("Text highlighting took: " + watch.ElapsedMilliseconds + " milliseconds");
+					watch.Stop();
                 });
-
-                System.Diagnostics.Debug.WriteLine("Text highlighting took: " + watch.ElapsedMilliseconds + " milliseconds");
-                watch.Stop();
             });
         }
 
@@ -471,7 +384,7 @@ namespace mobile_style_editor
 			UIKeyboard.Notifications.ObserveWillHide(OnKeyboardHide);            
         }
 
-		private void OnKeyboardShow(object sender, UIKeyboardEventArgs e)
+		void OnKeyboardShow(object sender, UIKeyboardEventArgs e)
 		{
 			// Need to calculate keyboard exact size due to Apple suggestions
 			ScrollEnabled = true;
@@ -481,7 +394,7 @@ namespace mobile_style_editor
 			ContentInset = inset;
 		}
 
-		private void OnKeyboardHide(object sender, UIKeyboardEventArgs e)
+		void OnKeyboardHide(object sender, UIKeyboardEventArgs e)
         {
             ContentInset = new UIEdgeInsets(0, 0, 0, 0);
 		}
