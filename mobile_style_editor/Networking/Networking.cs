c9ef6ce6ad23fc3c52ba3c3f2aa9b03c26950377
Aare Undo <aare@carto.com>
2018-01-18 14:10:48 +0200

    iOS.Droid: networking libraries and logic

---
+++ mobile_style_editor/Networking/Networking.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace mobile_style_editor.Networking
+{
+    public static class Networking
+    {
+        public static async Task<Stream> GetStyle(string name)
+        {
+            string baseUrl = "https://nutifront.s3.amazonaws.com/";
+            string folder = "style_editor_sample_styles/";
+            string url = baseUrl + folder + name + ".zip";
+
+            using (var client = new HttpClient())
+            {
+                HttpResponseMessage httpResponse = await client.GetAsync(url);
+
+                return await httpResponse.Content.ReadAsStreamAsync();
+            }
+        }
+    }
+}
